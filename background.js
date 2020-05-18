465dc859fb028ee0fe6f41b8850f54e0bdf7707e
CartierCodes <cburz@att.net>
2020-05-18 14:16:03 -0500

    f

--- background.js
+++ background.js
@@ -1,1 +1,18 @@
+let contextMenus = {};
 
+contextMenus.createWebSearch =
+    chrome.contextMenus.create(
+        {"title": "Search for this **SOMETHING**"},
+        function () {
+            if (chrome.runtime.lastError) {
+               console.error(chrome.runtime.lastError.message);
+            }
+});
+
+chrome.contextMenus.onClicked.addListener(contextMenuHandler);
+
+function contextMenuHandler(info, tab) {
+    if (info.menuItemID===contextMenus.createWebSearch) {
+        console.log("clicked on the webSearch thingy");
+    }
+}
