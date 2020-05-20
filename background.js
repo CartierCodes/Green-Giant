bf234249f95af6b8a53892491b7942ada4522ee5
CartierCodes <cburz@att.net>
2020-05-19 23:37:45 -0500

    changed js names

--- background.js
+++ background.js
@@ -1,18 +1,26 @@
 let contextMenus = {};
 
 contextMenus.webSearch = chrome.contextMenus.create(
-    {"title": "Search for this **SOMETHING**"},
-    function () { if (chrome.runtime.lastError) console.error(chrome.runtime.lastError.message); }
-);
+        {"title": "Check Amazon for this page",
+        "contexts" : ["editable"] },
+        function () { if (chrome.runtime.lastError) console.error(chrome.runtime.lastError.message); }
+    );
 
 chrome.contextMenus.onClicked.addListener(contextMenuHandler);
 
-function contextMenuHandler(info, tab) {
-    if (info.menuItemID === contextMenus.webSearch) {
-        if (tab.url.includes("amazon")) console.log("successeeeseseesse");
-        console.log("clicked on the webSearch thingy");
-        chrome.tabs.executeScript({
-            file: "webSearch.js"
-        });
+function contextMenuHandler(info, tab){
+    if (info.menuItemId === contextMenus.webSearch){
+        if (tab.url.includes("walmart")) {
+            console.log("this is walmart's page");
+            chrome.tabs.executeScript({
+                file: "compareWalmart.js"
+            });
+        }
+        else if (tab.url.includes("wayfair")) {
+            console.log("this is wayfair's page");
+            chrome.tabs.executeScript({
+                file: "compareWayfair.js"
+            });
+        }
     }
 }
