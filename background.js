9021ecf9caadc489488cd2efc0120e15b6cd688f
CartierCodes <cburz@att.net>
2020-05-19 19:28:35 -0500

    f

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
 let contextMenus = {};
 
-contextMenus.createWebSearch =
-    chrome.contextMenus.create(
-        {"title": "Search for this **SOMETHING**"},
-        function () {
-            if (chrome.runtime.lastError) {
-               console.error(chrome.runtime.lastError.message);
-            }
-});
+contextMenus.webSearch = chrome.contextMenus.create(
+    {"title": "Search for this **SOMETHING**"},
+    function () { if (chrome.runtime.lastError) console.error(chrome.runtime.lastError.message); }
+);
 
 chrome.contextMenus.onClicked.addListener(contextMenuHandler);
 
 function contextMenuHandler(info, tab) {
-    if (info.menuItemID===contextMenus.createWebSearch) {
+    if (info.menuItemID === contextMenus.webSearch) {
+        if (tab.url.includes("amazon")) console.log("successeeeseseesse");
         console.log("clicked on the webSearch thingy");
+        chrome.tabs.executeScript({
+            file: "webSearch.js"
+        });
     }
 }
